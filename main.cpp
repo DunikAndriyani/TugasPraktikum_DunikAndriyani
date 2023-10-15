a812480406289a3600c4d14a007ff4a77781f663
dunik <dunikandriyani@gmail.com>
2023-10-15 20:59:43 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "-------------------------------------------------------" << endl << endl;
+    cout << "Nama : Dunik Andriyani" << endl << endl;
+    cout << "NPM : 2310631170134" << endl << endl;
+    cout << "Kelas : B - Informatika" << endl<< endl;
+    cout << "Cita - Cita : Data Scientist" << endl << endl;
+    cout << "Motivasi Masuk Informatika : Penasaran dengan dunia IT " << endl << endl;
+    cout << "-------------------------------------------------------" << endl << endl;
+    return 0;
+}
