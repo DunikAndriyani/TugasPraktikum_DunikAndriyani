cffb82e03c03a10205893d7006d6735605fef134
DunikAndriyani <147916679+DunikAndriyani@users.noreply.github.com>
2023-10-22 17:53:03 +0700

    Tugas Praktikum Alpro 1B

--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 int main()
 {
+    int dividend,divisor,quotient,remainder;
 
-    cout << "-------------------------------------------------------" << endl << endl;
-    cout << "Nama : Dunik Andriyani" << endl << endl;
-    cout << "NPM : 2310631170134" << endl << endl;
-    cout << "Kelas : B - Informatika" << endl<< endl;
-    cout << "Cita - Cita : Data Scientist" << endl << endl;
-    cout << "Motivasi Masuk Informatika : Penasaran dengan dunia IT " << endl << endl;
-    cout << "-------------------------------------------------------" << endl << endl;
+    cout<<"Enter Dividend: ";
+    cin>>dividend;
+    cout<<"Enter Divisor: ";
+    cin>>divisor;
+
+    quotient=dividend/divisor;
+    remainder=dividend%divisor;
+
+    cout<<"Quotient = "<<quotient<<endl;
+    cout<<"Remainder = "<<remainder;
+
     return 0;
 }
---
+++ tugas.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout << "firstValue =";cin>>a;
+    cout << "secondValue =";cin>>b;
+    cout << "Value =";cin>>c;
+    cout << "firstValue ="<<c<<endl;
+    cout << "secondValue ="<<a<<endl;
+    cout << "Value ="<<b<<endl;
+    cout << "firstValue ="<<b<<endl;
+    cout << "secondValue ="<<a<<endl;
+    cout << "Value ="<<c<<endl;
+
+
+
+    cout << "firstValue =";cin>>a;
+    cout << "secondValue =";cin>>b;
+    cout << "firstValue ="<<b<<endl;
+    cout << "secondValue ="<<a<<endl;
+
+    return 0;
+}
